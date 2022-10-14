3cbf94bd0e5fef151822876fae1a97e775dd2f00
naimulh247 <naimulh247@gmail.com>
2022-10-14 10:45:16 -0400

    added installer

---
+++ install.sh
@@ -0,0 +1,24 @@
+!#/bin/bash
+sudo apt update
+
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common libssl-dev libffi-dev git wget nano
+
+sudo groupadd docker
+
+sudo usermod -aG docker $USER
+
+curl -fsSl https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add
+
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
+
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+
+sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove -y
+
+sudo apt-get install -y docker-ce containerd.io && sudo apt-get install -y kubectl
+
+echo "sudo service docker start" >> ~/.profile
+
+exit
